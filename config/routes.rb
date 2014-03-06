ca4c95cec33f576ca85fd45ac3aaf69cc0310d9d
Peter T Bosse II <ptb@ioutime.com>
2014-03-06 17:13:47 -0500

    rails g scaffold user first_name last_name; rake db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # encoding: UTF-8
 ChaplinRest::Application.routes.draw do
+  resources :users
+
   root to: 'application#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
